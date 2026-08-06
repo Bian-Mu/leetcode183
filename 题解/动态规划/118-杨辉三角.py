c79ed3db85cbbcb57c893e6d56143b834a4f56aa
Bian-Mu <2570772184@qq.com>
2026-08-06 20:30:34 +0800

    do: 1 q

--- 题解/动态规划/118-杨辉三角.py
+++ 题解/动态规划/118-杨辉三角.py
@@ -15,3 +15,15 @@ class Solution:
         ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j]
 
     return ans
+
+  def mysolution(self, numRows:int) -> list[list[int]]:
+    dp=[]
+    
+    for i in range(numRows):
+      dp.append(([1] * (i+1)))
+    
+    for i in range(2,numRows):
+      for j in range(1,i):
+        dp[i][j]=dp[i-1][j-1]+dp[i-1][j]
+    
+    return dp
